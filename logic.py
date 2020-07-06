e2ae2017d20f961625ffd30398eb1707025b43b9
Shivansh <shivanshgoel21@gmail.com>
2020-07-06 18:30:42 +0100

    bud fixed

--- logic.py
+++ logic.py
@@ -2,38 +2,48 @@
 class Logic:
     def __init__(self,player_name):
         self.player=player_name
-
-    nums={
-        "00" : 0,
-        "01" : 1,
-        "02" : 2,
-        "10" : 3,
-        "11" : 4,
-        "12" : 5,
-        "21" : 6,
-        "22" : 7,
-        "23" : 8,
-    }
+        self.nums={
+            "00" : 0,
+            "01" : 1,
+            "02" : 2,
+            "10" : 3,
+            "11" : 4,
+            "12" : 5,
+            "20" : 6,
+            "21" : 7,
+            "22" : 8,
+        }
 
     def check(self):
         nums=self.nums
+        for i in range(3):
+            for j in range(3):
+                print(nums[f"{i}{j}"])
+        # row 0 
         if(nums["00"]==nums["01"]==nums["02"]):
             return f"{self.player} Won!!"
-
+        # row 1
         elif(nums["10"]==nums["11"]==nums["12"]):
             return f"{self.player} Won!!"
-        
-        elif(nums["21"]==nums["22"]==nums["23"]):
+        # row 2
+        elif(nums["20"]==nums["21"]==nums["22"]):
             return f"{self.player} Won!!"
-
-        elif(nums["00"]==nums["12"]==nums["23"]):
+        # diagonal
+        elif(nums["00"]==nums["11"]==nums["22"]):
             return f"{self.player} Won!!"
-        
-        elif(nums["02"]==nums["12"]==nums["21"]):
+        # diagonal
+        elif(nums["02"]==nums["11"]==nums["20"]):
             return f"{self.player} Won!!"
-            
-  ## even more logic
-
+        # column 1
+        elif(nums["00"]==nums["11"]==nums["21"]):
+            return f"{self.player} Won!!"
+        # column 2
+        elif(nums["01"]==nums["11"]==nums["21"]):
+            return f"{self.player} Won!!"
+        # column 3
+        elif(nums["02"]==nums["12"]==nums["22"]):
+            return f"{self.player} Won!!"
+## even more logic
 
 ##debugging
 # ll=Logic("Shivansh")
--- widgets.py
+++ widgets.py
@@ -5,13 +5,22 @@ from logic import Logic
 
 player1=Logic("Player1")
 player2=Logic("Player2")
-player=''
+player='Player1'
+game_result=None
 
-
-def get_player_name(player_name):
-    global player
-    player=player_name
-    print(player_name)
+def get_player_name(main_frame):
+    global player,game_result
+    player_name=tk.StringVar()
+    if game_result is not None:
+        global player1,player2
+        player_name.set(game_result)
+        game_result=None
+        player1=Logic("Player1")
+        player2=Logic("Player2")
+    else:
+        player_name.set(f"{player}'s Turn")
+    print(player_name.get())
+    tk.Label(main_frame,textvariable=player_name,bg='#d93253',font=("helvetica 12",12)).grid(row=7,column=1)
 
 class Widgets:
     def __init__(self):
@@ -29,27 +38,29 @@ class Widgets:
     def new_grid_button(self,frame,text,width,height,bg_color,row,column):
         global player1,player2
         self.click_count=0
+        self.frame=frame       
         ## when button is clicked
         def clicked(i,j):
+            global game_result
             ## debugging
-            print(i,j)
-            print(self.btn)
+            # print(i,j)
+            # print(self.btn)
             if(self.click_count==0):
-                print(f"Click count {self.click_count}")
                 if(player=="Player1"):
                     player1.nums[f"{i}{j}"]="x"
                     print(player1.check())
+                    game_result=player1.check()
+                    get_player_name(self.frame)
                     self.click_count+=1
-                    
                 elif(player=="Player2"):
                     player2.nums[f"{i}{j}"]="o"
                     print(player2.check())
+                    game_result=player2.check()
+                    get_player_name(self.frame)
                     self.click_count+=1
                 self.used_by_player=player
                 self.configure_button()
             else:
                 print(f"Already Used by {self.used_by_player}")
-            
-
-        self.btn=tk.Button(frame,text=text,width=width,height=height,bg=bg_color,command= lambda : clicked(row,column))
+        self.btn=tk.Button(frame,text=text,font="helvetica 12",width=width,height=height,bg=bg_color,command= lambda : clicked(row,column))
         self.btn.grid(row=row, column=column)
